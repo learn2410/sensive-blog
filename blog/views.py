33a1d2b6974a7ab5e3a97cf8a2c9fa69f0a1b927
learn2410 <learn2410@mail.ru>
2022-08-02 15:33:59 +0500

    replace sorted func with order_by

--- blog/views.py
+++ blog/views.py
@@ -32,9 +32,7 @@ def serialize_tag(tag):
 
 def index(request):
 
-    posts = Post.objects.annotate(likes_count=Count('likes'))
-    popular_posts=sorted(posts,key=get_likes_count,reverse=True)
-    most_popular_posts = popular_posts[:5]
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     fresh_posts = Post.objects.order_by('-published_at')
     most_fresh_posts = list(fresh_posts)[:5]
@@ -84,7 +82,7 @@ def post_detail(request, slug):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     context = {
         'post': serialized_post,
@@ -103,7 +101,7 @@ def tag_filter(request, tag_title):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     related_posts = tag.posts.all()[:20]
 

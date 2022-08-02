05a2dd3bd1dd0c4c24c7103f1a6392df77bb6c80
learn2410 <learn2410@mail.ru>
2022-08-02 12:34:27 +0500

    fix sorting in main page

--- blog/views.py
+++ blog/views.py
@@ -33,15 +33,15 @@ def serialize_tag(tag):
 def index(request):
 
     posts = Post.objects.annotate(likes_count=Count('likes'))
-    popular_posts=sorted(posts,key=get_likes_count)
-    most_popular_posts = popular_posts[-5:]
+    popular_posts=sorted(posts,key=get_likes_count,reverse=True)
+    most_popular_posts = popular_posts[:5]
 
-    fresh_posts = Post.objects.order_by('published_at')
-    most_fresh_posts = list(fresh_posts)[-5:]
+    fresh_posts = Post.objects.order_by('-published_at')
+    most_fresh_posts = list(fresh_posts)[:5]
 
     tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = sorted(tags, key=get_related_posts_count,reverse=True)
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [

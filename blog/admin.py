ccefa466c0b3d46f6ce118709fb676aefe82a2e0
learn2410 <learn2410@mail.ru>
2022-08-18 00:16:02 +0500

    fix admin pages load time

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.db.models import Prefetch
+
 from blog.models import Post, Tag, Comment
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    list_display = ('id','title','published_at')
+    # list_filter = ['author']
+    raw_id_fields = ('author', 'tags', 'likes')
 
-admin.site.register(Post)
 admin.site.register(Tag)
-admin.site.register(Comment)
+
+
+class Ptrfetch(object):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    raw_id_fields = ('author', 'post')
+    list_display = ('post','author','text','published_at')
+    search_fields = ('text',)
+    ordering=('pk',)
+    # list_filter = ['post','author']
+
+    def get_queryset(self, request):
+        queryset = super(CommentAdmin, self).get_queryset(request)
+        queryset = queryset.prefetch_related(
+            Prefetch('author',queryset=User.objects.only('username')),
+            Prefetch('post',queryset=Post.objects.only('title')))
+        return queryset
